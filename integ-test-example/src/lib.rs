f04c83f628f9685c678b7ca0e6c7b6bc0355f046
Rock Okechukwu <rockok10@gmail.com>
2024-06-04 16:22:27 +0100

    Add markdown documentation file

--- integ-test-example/src/lib.rs
+++ integ-test-example/src/lib.rs
@@ -6,6 +6,12 @@ use std::process;
 
 /// This function gets the process ID of the current
 /// executable. it returns a non-zero number
+/// ```
+/// fn get_id() {
+/// let x = integ_test_example::get_process_id();
+/// println!("{}",x);
+/// }
+/// ```
 
 pub fn get_process_id()-> u32{
     process::id()
