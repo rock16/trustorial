847f0afbaa2fe858ee6423f85e377b5a20479244
Rock Okechukwu <rockok10@gmail.com>
2024-06-04 16:00:07 +0100

    add documentation to integration test library lib.rs file

--- integ-test-example/src/lib.rs
+++ integ-test-example/src/lib.rs
@@ -1,4 +1,11 @@
+//! This is a library that contains functions related to 
+//! dealing with process,
+//! and makes these tasks more convenient.
+
 use std::process;
+
+/// This function gets the process ID of the current
+/// executable. it returns a non-zero number
 
 pub fn get_process_id()-> u32{
     process::id()
